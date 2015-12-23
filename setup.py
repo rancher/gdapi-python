4ec8258361c88c5854d21b2b1be881841f8c7eb7
Andreas Stenius <andreas.stenius@svenskaspel.se>
2015-12-23 08:33:59 +0100

    setup.py: go with the flow

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import os
 from setuptools import setup
 
+# https://caremad.io/2013/07/setup-vs-requirement/
+
 with open('./requirements.txt') as r:
-    requirements = [line for line in r]
+    # strip fixed version info from requirements file
+    requirements = [line.split('=', 1)[0] for line in r]
 
 setup(
     name='gdapi-python',
-    version='0.5.1',
+    version='0.5.2',
     py_modules=['gdapi'],
     url='https://github.com/godaddy/gdapi-python',
     license='MIT Style',
